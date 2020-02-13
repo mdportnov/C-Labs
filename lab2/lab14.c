39b523f7e97d68765c6b168e3ce554af0a9f398f
Mikhail Balykin <mikport26@gmail.com>
2020-02-14 00:28:30 +0300

    second (successful) attempt of 2nd lab: with matrix structure

--- lab2/lab14.c
+++ lab2/lab14.c
@@ -3,7 +3,7 @@
 
 typedef struct line {
     int n;
-    double *ar;
+    int *arr;
 } line;
 
 typedef struct matrix {
@@ -11,36 +11,105 @@ typedef struct matrix {
     line *pt;
 } matrix;
 
+int getInt(int *a);
+
+void input(int *m, struct matrix *matrix);
+
+void printV(int *vec, int size);
+
+void print(matrix rm)
+{
+    int i,j;
+    for(i=0;i<rm.m;i++)
+    {
+        for(j=0;j<rm.pt[i].n;j++)
+        {
+            printf("%d ", rm.pt[i].arr[j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+void erase(matrix *a)
+{
+    int i;
+    for (i = 0; i < a->m; ++i)
+        free(a->pt[i].arr);
+    free(a->pt);
+    a->m = 0;
+    a->pt = NULL;
+}
+
+int sum_in_row(int* arr_ptr, int length)
+{
+    int sum =0;
+    for (int i = 0; i < length; i++)
+    {
+        sum += *(arr_ptr + i);
+    }
+    return sum;
+}
+
 
 int main() {
-    int m, n;
-    scanf("%d", &m);
+    int m;
+    struct matrix matrix;
 
-    int **matrix = (int **) malloc(m * sizeof(int *));
-
-    for (int i = 0; i < m; i++) {
-        printf("Input quantity of elements in %d row\n", i + 1);
-        scanf("%d", &n);
-        matrix[i] = (int *) malloc(n * sizeof(int));
-        printf("Input %d row\n", i + 1);
-        for (int j = 0; j < n; j++) {
-            scanf("%d", &matrix[i][j]);
-        }
-    }
+    input(&m, &matrix); // ввод матрицы
 
     printf("It's your matrix: \n");
-//    for (int i = 0; i < m; i++) {
-//        for (int j = 0; j < sizeof(matrix[i])/sizeof(int); j++) {
-//            printf("%d", matrix[i][j]);
-//        }
-//        printf("\n");
-//    }
+    print(matrix); // вывод матрицы
 
-    for (int i = m - 1; i > 0; i--) {
-        free(matrix[i]);
+    printf("It's vector of your matrix: \n");
+
+    int* vector=(int *)malloc(m*sizeof(vector));
+    for (int k = 0; k < m; k++) { // заполнение вектора
+        *(vector+k)=sum_in_row(matrix.pt[k].arr, matrix.pt[k].n);
     }
-    free(matrix);
 
+    printV(vector, m); // вывод вектора
 
+    erase(&matrix); // чистим память
     return 0;
+}
+
+void printV(int *vec, int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", vec[i]);
+    }
+}
+
+void input(int *m, struct matrix *matrix) {
+    int n;
+    getInt(m);
+    (*matrix).m= (*m);
+    (*matrix).pt=(line*)calloc((*m), sizeof(line));
+
+    for (int i = 0; i < (*m); ++i) {
+        printf("Input quantity of elements in %d row\n", i + 1);
+        getInt(&n);
+        (*matrix).pt[i].n=n;
+        (*matrix).pt[i].arr=(int*)calloc(n, sizeof(int));
+        printf("Input %d row\n", i + 1);
+
+        for (int j = 0; j < n; j++) {
+            getInt(&(*matrix).pt[i].arr[j]);
+        }
+    }
+}
+
+int getInt(int *a)
+{
+    int n;
+    do{
+        n = scanf("%d", a);
+        if (n < 0)
+            return 0;
+        if (n == 0){
+            printf("%s\n", "Error! Repeat input");
+            scanf("%*c", 0);
+        }
+    } while (n == 0);
+    return 1;
 }
